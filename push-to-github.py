2ed1648424c9129e81ee49381ace44e46f963fff
lyhue1991 <lyhue1991@163.com>
2022-01-08 07:52:46 +0800

    revise some chapters

--- push-to-github.py
+++ push-to-github.py
@@ -19,13 +19,15 @@
 
 # !git remote rm origin 
 
-# !git remote add origin https://github.com/lyhue1991/eat_tensorflow2_in_30_days
+# !git remote add origin git@github.com:lyhue1991/eat_tensorflow2_in_30_days.git
 
 # !git remote add gitee https://gitee.com/Python_Ai_Road/eat_tensorflow2_in_30_days
 
-# !git pull  origin master 
+# +
+# #!git pull  origin master 
+# -
 
-# !git push  -f origin master 
+# !git push   origin master 
 
 # !git push -f gitee master 
 
